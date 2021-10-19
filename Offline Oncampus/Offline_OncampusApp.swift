6da97aadc821835d793bcbc18f4f8c595c7d9906
Andrew Smith <asmith@ves.org>
2021-10-19 13:23:21 -0400

    Initial Commit

---
+++ Offline Oncampus/Offline_OncampusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Offline_OncampusApp.swift
+//  Offline Oncampus
+//
+//  Created by admin on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Offline_OncampusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
