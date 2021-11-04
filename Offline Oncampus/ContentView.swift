e6bf0fa0fcd7b1d76b32bd6e695ee9b829ca7384
Andrew Smith <asmith@ves.org>
2021-11-04 18:52:19 -0400

    Mostly button stuff

--- Offline Oncampus/ContentView.swift
+++ Offline Oncampus/ContentView.swift
@@ -7,15 +7,27 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    @State var day: String = "Blank"
+    
     var body: some View {
-        ZStack {
-            Image("Loading screen OO")
-                .offset(x: -17, y: -50)
+        NavigationView{
+    
+            ZStack {
+                Image("Logo OO")
+                .offset(x: 0, y: -100)
+                .scaledToFit()
+                
             HStack{
-                Button("Sun", action:{ print("to do")}) .font(.title)
+                Button("Sun", action:{
+                    self.day = "Sun"
+                    print(day)
+                }) .font(.title)
                     .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                Button("Mon", action:{ print("to do")}) .font(.title)
+                Button("Mon", action:{ var _: String = "Mon"}) .font(.title)
                     .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
                 Button("Tue", action:{ print("to do")}) .font(.title)
                     .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
@@ -27,8 +39,15 @@ struct ContentView: View {
                     .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
                 Button("Sat", action:{ print("to do")}) .font(.title)
                     .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-    
-            }.offset(x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/-300.0/*@END_MENU_TOKEN@*/)
+            }.frame(width: 500, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("AccentColor")/*@END_MENU_TOKEN@*/).offset(x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: -370.0)
+            HStack{
+                NavigationLink(
+                    destination: SwiftUIView1(),
+                    label: {
+                        Text("Sun").font(.title)
+                            .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    })
+            }
             HStack{
                 Button("Asignments", action:{ print("to do")}) .font(.title)
                     .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
@@ -37,18 +56,17 @@ struct ContentView: View {
                 Button("Menus", action:{ print("to do")}) .font(.title)
                     .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
                 
-            }.offset(x: 0, y: 300)
+            }.offset(x: 0, y: 250)
             Button("Download", action:{ print("to do")}) .font(.title)
                 .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
                 .offset(x: 0, y: 50)
-        }
-        
-        
-        
-      
+            }.frame(width: .infinity, height: 800).background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.gray/*@END_MENU_TOKEN@*/)
             
+        
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
