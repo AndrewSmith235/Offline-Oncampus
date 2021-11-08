2d7c9cdd84bfc331c7c4d2afffae7cf5dc3f71da
Andrew Smith <asmith@ves.org>
2021-11-08 14:10:14 -0500

    11/8/2021

--- Offline Oncampus/ContentView.swift
+++ Offline Oncampus/ContentView.swift
@@ -15,57 +15,78 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView{
-    
+            
             ZStack {
                 Image("Logo OO")
-                .offset(x: 0, y: -100)
-                .scaledToFit()
+                    .offset(x: 0, y: -100)
+                    .scaledToFit()
                 
-            HStack{
-                Button("Sun", action:{
-                    self.day = "Sun"
-                    print(day)
-                }) .font(.title)
-                    .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                Button("Mon", action:{ var _: String = "Mon"}) .font(.title)
-                    .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                Button("Tue", action:{ print("to do")}) .font(.title)
-                    .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                Button("Wed", action:{ print("to do")}) .font(.title)
-                    .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                Button("Thu", action:{ print("to do")}) .font(.title)
-                    .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                Button("Fri", action:{ print("to do")}) .font(.title)
-                    .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                Button("Sat", action:{ print("to do")}) .font(.title)
-                    .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-            }.frame(width: 500, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("AccentColor")/*@END_MENU_TOKEN@*/).offset(x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: -370.0)
-            HStack{
-                NavigationLink(
-                    destination: SwiftUIView1(),
-                    label: {
-                        Text("Sun").font(.title)
-                            .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                    })
+                /*HStack{
+                 Button("Sun", action:{
+                 self.day = "Sun"
+                 print(day)
+                 }) .font(.title)
+                 .accentColor(.white)
+                 Button("Mon", action:{ var _: String = "Mon"}) .font(.title)
+                 .accentColor(.white)
+                 Button("Tue", action:{ print("to do")}) .font(.title)
+                 .accentColor(.white)
+                 Button("Wed", action:{ print("to do")}) .font(.title)
+                 .accentColor(.white)
+                 Button("Thu", action:{ print("to do")}) .font(.title)
+                 .accentColor(.white)
+                 Button("Fri", action:{ print("to do")}) .font(.title)
+                 .accentColor(.white)
+                 Button("Sat", action:{ print("to do")}) .font(.title)
+                 .accentColor(.white)
+                 }.frame(width: 500, height: 50, alignment: .center/*@END_MENU_TOKEN@*/).background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("AccentColor")/*@END_MENU_TOKEN@*/).offset(x: /*@START_MENU_TOKEN@*/0.0, y: -370.0)
+                 HStack{
+                 NavigationLink(
+                 destination: SwiftUIView1(),
+                 label: {
+                 Text("Sun").font(.title)
+                 .accentColor(.white)
+                 })
+                 }
+                 HStack{
+                 Button("Asignments", action:{ print("to do")}) .font(.title)
+                 .accentColor(.white)
+                 Button("Schedule", action:{ print("to do")}) .font(.title)
+                 .accentColor(.white)
+                 Button("Menus", action:{ print("to do")}) .font(.title)
+                 .accentColor(.white)
+                 
+                 }.offset(x: 0, y: 250)
+                 Button("Download", action:{ print("to do")}) .font(.title)
+                 .accentColor(.white)
+                 .offset(x: 0, y: 50)*/
+              
+                TabView {
+                    Text("Sun")
+                        .font(.system(size: 30, weight: .bold, design: .rounded))
+                        .tabItem {
+                            Text("SUN")
+                                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                                .fontWeight(.bold)
+                        }
+                }
+                HStack{
+                    Text("S")
+                    Text("M")
+                    Text("T")
+                    Text("W")
+                    Text("T")
+                    Text("F")
+                    Text("S")
+                }
+                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                }
             }
-            HStack{
-                Button("Asignments", action:{ print("to do")}) .font(.title)
-                    .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                Button("Schedule", action:{ print("to do")}) .font(.title)
-                    .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                Button("Menus", action:{ print("to do")}) .font(.title)
-                    .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                
-            }.offset(x: 0, y: 250)
-            Button("Download", action:{ print("to do")}) .font(.title)
-                .accentColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
-                .offset(x: 0, y: 50)
-            }.frame(width: .infinity, height: 800).background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.gray/*@END_MENU_TOKEN@*/)
             
-        
+            
         }
     }
-}
+
 
 
 struct ContentView_Previews: PreviewProvider {
