6a0c26818c7f692cb36ebf97e224f4498cdb14ef
Andrew Smith <asmith@ves.org>
2021-12-03 10:21:30 -0500

    Api stuff

--- Offline Oncampus/ContentView.swift
+++ Offline Oncampus/ContentView.swift
@@ -39,7 +39,7 @@ struct ContentView: View {
                  .accentColor(.white)
                  Button("Sat", action:{ print("to do")}) .font(.title)
                  .accentColor(.white)
-                 }.frame(width: 500, height: 50, alignment: .center/*@END_MENU_TOKEN@*/).background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("AccentColor")/*@END_MENU_TOKEN@*/).offset(x: /*@START_MENU_TOKEN@*/0.0, y: -370.0)
+                 }.frame(width: 500, height: 50, alignment: .center/*@END_MENU_TOKEN@*/).background(Color("AccentColor")).offset(x: /*@START_MENU_TOKEN@*/0.0, y: -370.0)
                  HStack{
                  NavigationLink(
                  destination: SwiftUIView1(),
@@ -60,79 +60,134 @@ struct ContentView: View {
                  Button("Download", action:{ print("to do")}) .font(.title)
                  .accentColor(.white)
                  .offset(x: 0, y: 50)*/
-              
+                
                 TabView {
-                    Text("Mon")
-                        .font(.system(size: 30, weight: .bold, design: .rounded))
+                    VStack{
+                       Text("Computer Science"+" 8:45")
+                        //Text("varScheduleItem1"+" varScheduleTime1")
+                    }//modifiers to make look nice
                         .tabItem {
                             Text("MON")
-                                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                                .font(.largeTitle)
                                 .fontWeight(.bold)
                         }
                     Text("Tue")
                         .font(.system(size: 30, weight: .bold, design: .rounded))
                         .tabItem {
                             Text("TUE")
-                                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                                .font(.largeTitle)
                                 .fontWeight(.bold)
                         }
                     Text("Wed")
                         .font(.system(size: 30, weight: .bold, design: .rounded))
                         .tabItem {
                             Text("WED")
-                                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                                .font(.largeTitle)
                                 .fontWeight(.bold)
                         }
                     Text("Thu")
                         .font(.system(size: 30, weight: .bold, design: .rounded))
                         .tabItem {
                             Text("THU")
-                                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                                .font(.largeTitle)
                                 .fontWeight(.bold)
                         }
                     Text("Fri")
                         .font(.system(size: 30, weight: .bold, design: .rounded))
                         .tabItem {
                             Text("FRI")
-                                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                                .font(.largeTitle)
                                 .fontWeight(.bold)
                         }
                 }
                 HStack{
-                HStack{
-                    Spacer()
-                    Text("M")
-                    Spacer()
-                    Spacer()
-                    Text("T")
-                    Spacer()
-                    Spacer()
-                    
-                }
-                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
-                .offset( y: 250)
-                HStack{
-                    Text("W")
-                }.font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
-                .offset(x: 0, y: 250)
-                HStack{
-                    Spacer()
-                    Spacer()
-                    Text("T").offset(x: -5)
-                    Spacer()
-                    Spacer()
-                    Text("F")
-                    Spacer()
-                }
-                .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
-                .offset(x: 0, y: 250)
+                    HStack{
+                        Spacer()
+                        Text("M")
+                        Spacer()
+                        Spacer()
+                        Text("T")
+                        Spacer()
+                        Spacer()
+                        
+                    }
+                    .font(.largeTitle)
+                    .offset( y: 250)
+                    HStack{
+                        Text("W")
+                    }.font(.largeTitle)
+                    .offset(x: 0, y: 250)
+                    HStack{
+                        Spacer()
+                        Spacer()
+                        Text("T").offset(x: -5)
+                        Spacer()
+                        Spacer()
+                        Text("F")
+                        Spacer()
+                    }
+                    .font(.largeTitle)
+                    .offset(x: 0, y: 250)
                 }
             }
-            }
+        }
             
             
         }
     }
+
+
+
+class ViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        let url = "https://api.sunrise-sunset.org/json"
+        getdata(from: url)
+    }
+
+    private func getdata(from url: String) {
+        URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
+        guard let data = data,  error == nil else {
+            print("error")
+            return
+        }
+            
+            var result: Response?
+            do{
+                result = try JSONDecoder().decode(Response.self, from: data)
+            }
+            catch{
+                print("failed to convert \(error.localizedDescription)")
+            }
+            guard let json = result else {
+                return
+            }
+            print(json.status)
+            print(json.results.sunrise)
+            
+        }).resume()
+}
+}
+struct Response: Codable {
+    let results: MyResult
+    let status: String
+}
+
+struct MyResult: Codable {
+    let sunrise: String
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -142,3 +197,4 @@ struct ContentView_Previews: PreviewProvider {
             .previewDevice("iPhone 8")
     }
 }
+
